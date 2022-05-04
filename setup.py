67f568ebde0e630749162912aea047f99c0aaa4f
Telettubi <101152860+Telettubi@users.noreply.github.com>
2022-05-04 10:49:49 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+  include_package_data = True,
+  name='calculgood',
+  version='0.0.1',
+  description='oss my calculator',
+  author='sua'
+  author_email='cookie3123@naver.com',
+  url='https://github.com/Telettubi/testcalcul/',
+  install_requires=['pytest'],
+  long_description='oss my calculator test python module',
+  long_description_content_type='test/markdown',
+  classifiers=[
+    "Programming Language :: Python :: 3",
+    "Operation System :: OS Independent",
+  ],
+)
